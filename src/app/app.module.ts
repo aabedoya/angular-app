974a5a7a0f49993f97d847d52714212430c60356
Alvaro Andrés Bedoya Gómez <andresbedoyagomez@gmail.com>
2022-03-24 09:36:12 -0500

    Proyecto con Angular 13 varios skills

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { FormNewItemComponent } from './form-new-item/form-new-item.component';
+import { CitiesComponent } from './cities/cities.component';
+import { FilterPipe } from './pipes/filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    FormNewItemComponent,
+    CitiesComponent,
+    FilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
